a955e6515e7e0394b63820bd22606d6c562f52a7
Abdulrahmanabuziad <budiielsayed@gmail.com>
2023-08-11 10:48:06 -0700

    xd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+o#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a-z in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	cha
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+		return (0);
+		r ch = 'z';
+}
