4a0ab99df4a2ee341e6b9b28c7776ef1f5cd3f95
Abdulrahmanabuziad <budiielsayed@gmail.com>
2023-08-11 11:01:25 -0700

    xd

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -12,7 +10,7 @@
 
 int main(void)
 {
-	cha
+	char ch = 'z';
 
 	while (ch >= 'a')
 	{
@@ -22,5 +20,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-r ch = 'z';
 }
